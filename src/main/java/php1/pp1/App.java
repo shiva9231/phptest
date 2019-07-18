83f6d3ce40026a83e495ad330cd6ea8021e4cfdb
Shiva Kumar <shiva.shanrohi@gmail.com>
2019-07-18 18:04:50 +0530

    test code

---
+++ src/main/java/php1/pp1/App.java
@@ -0,0 +1,13 @@
+package php1.pp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/php1/pp1/AppTest.java
@@ -0,0 +1,21 @@
+package php1.pp1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
